16475e37965f34a6753de769208bdc9f1d439dad
julio-sa <157306928+julio-sa@users.noreply.github.com>
2024-09-09 17:24:15 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,57 @@
+let noticias = {
+    titulo: "Notícias",
+    descricao: "Aqui você encontra as notícias mais recentes do Verdão",
+    noticias:"<p><li><a href='https://www.lance.com.br/brasileirao/stjd-julgara-quatro-atletas-por-briga-em-palmeiras-x-sao-paulo-veja-possiveis-punicoes.html'target='_blank'>STJD julgará quatro atletas por briga em Palmeiras x São Paulo; veja possíveis punições</a></li></p>" +
+                "<p><li><a href='https://www.lance.com.br/palmeiras/estevao-do-palmeiras-tem-media-superior-a-neymar-em-primeiro-ano-como-profissional.html'target='_blank'>Estêvão, do Palmeiras, tem média superior a Neymar em primeiro ano como profissional</a></li><p>" +
+                "<p><li><a href='https://www.lance.com.br/selecao-brasileira/estevao-revela-frio-na-barriga-ao-encontrar-idolo-na-selecao-brasileira.html'target='_blank'>Estêvão revela frio na barriga ao encontrar ídolo na Seleção Brasileira</a></li></p>",
+    link : "www.lance.com.br/Palmeiras",
+    tags: "noticias atualizacoes atualizaçoes atualizações"
+};
+
+let historia = {
+    titulo: "História",
+    descricao: "<p>A história do Palmeiras, um dos clubes mais vencedores do Brasil, pode ser resumida da seguinte forma:</p>"+
+                "<p><li>Fundação: O Palmeiras foi fundado em 26 de agosto de 1914 por imigrantes italianos, com o nome de Società Sportiva Palestra Italia.</li></p>"+
+                "<p><li>Mudança de nome: Em 1942, devido à Segunda Guerra Mundial, o clube foi obrigado a mudar de nome para Sociedade Esportiva Palmeiras.</li></p>"+
+                "<p><li>Títulos: O Palmeiras é o maior campeão nacional do Brasil, com 18 títulos. Entre as suas maiores conquistas estão o Mundial Interclubes de 1951, as Copas Libertadores da América de 1999, 2020 e 2021, 12 Campeonatos Brasileiros e 4 Copas do Brasil.</li></p>"+
+                "<p><li>Mascotes: O Palmeiras tem dois mascotes oficiais: o porco e o periquito.</li></p>"+
+                "<p><li>Torcida: O Palmeiras tem mais de 15 milhões de torcedores.</li></p>"+
+                "<p><li>Ídolos: O Palmeiras tem muitos ídolos, como Cafu, Marcos, César Maluco e Alex.</li></p>"+
+                "<p><li>Estádio: O estádio do Palmeiras é o Allianz Parque.</li></p>"+
+                "<p><li>Uniforme: O uniforme do Palmeiras foi alterado porque as cores vermelha, branca e verde faziam referência à bandeira italiana.</li></p>"+
+                "<p><li>Símbolo: O símbolo do Palmeiras é uma inscrição “Palmeiras” em branco e as oito estrelas, que fazem referência ao mês de fundação do Palestra</li></p>",
+    tags: "historia história resumo fundação escudo"
+};
+
+let camisa1 = {
+    titulo : "Camisa I Jogador Temporada 24/25",
+    descricao : `
+        <a href="https://www.palmeirasstore.com/camisa-palmeiras-puma-i-24-25---jogador-777237-01_pai/p" target='_blank'>
+            <div>
+                <img src="camisa1_24.25.jpeg" alt="Camisa 1">
+                <p>Camisa principal do time na temporada 24/25</p>
+            </div>
+        </a>
+    `,
+    link : "https://www.palmeirasstore.com/camisa-palmeiras-puma-i-24-25---jogador-777237-01_pai/p",
+    tags : "camisa camisas principal primeira verde uniforme"
+};
+
+let camisa2 = {
+    titulo : "Camisa II Jogador Temporada 24/25",
+    descricao : `
+        <a href="https://www.palmeirasstore.com/camisa-palmeiras-puma-ii-24-25-jogador-777231-01_pai/p" target= '_blank'>
+            <div>
+                <img src="camisa2_24.25.jpeg" alt="Camisa 2">
+                <p>Camisa secundária do time na temporada 24/25</p>
+            </div>
+        </a>
+    `,
+    link : "https://www.palmeirasstore.com/camisa-palmeiras-puma-ii-24-25---jogador-777231-01_pai/p",
+    tags: "camisa camisas secundária secundaria segunda branca uniforme"
+}
+
+let dados = [noticias
+    ,historia
+    ,camisa1
+    ,camisa2]
